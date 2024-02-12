6b0c0b1030b492b5acfffb7976ea90837ac5995e
Cuma Bolat <cuma.bolat@toptal.com>
2024-02-12 23:32:26 +0300

    change project structure

---
+++ src/main/java/com/cumabolat/kafatech/models/Student.java
@@ -0,0 +1,13 @@
+package com.cumabolat.kafatech.models;
+
+public class Student {
+    private String name;
+    private int age;
+    private String studentId;
+
+    public Student(String name, int age, String studentId) {
+        this.name = name;
+        this.age = age;
+        this.studentId = studentId;
+    }
+}
