351d3597549da42a099bcc2f03f34fc32145bd34
Cuma Bolat <cuma.bolat@toptal.com>
2024-02-12 23:52:54 +0300

    create student model, add necessary dependencies

--- src/main/java/com/cumabolat/kafatech/models/Student.java
+++ src/main/java/com/cumabolat/kafatech/models/Student.java
@@ -1,13 +1,57 @@
 package com.cumabolat.kafatech.models;
 
-public class Student {
-    private String name;
-    private int age;
-    private String studentId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-    public Student(String name, int age, String studentId) {
-        this.name = name;
-        this.age = age;
-        this.studentId = studentId;
-    }
+@Entity
+public class Student {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String lastName;
+  private int grade;
+
+  public Student() {
+  }
+
+  public Student(String name, String lastName, int grade) {
+    this.name = name;
+    this.lastName = lastName;
+    this.grade = grade;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public int getGrade() {
+    return grade;
+  }
+
+  public void setGrade(int grade) {
+    this.grade = grade;
+  }
 }
