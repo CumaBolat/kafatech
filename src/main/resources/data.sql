a9c324ca072b927e70b13fd81d2b7c79b9777ac6
Cuma Bolat <cuma.bolat@toptal.com>
2024-02-15 12:43:14 +0300

    configure database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO Student (name, last_name, grade) VALUES
+('Fatih', 'Çakır', 100),
+('Kafa', 'Tech', 100),
+('Student', 'Three', 95),
+('Student', 'Four', 88),
+('Student', 'Five', 92),
+('Student', 'Six', 87),
+('Student', 'Seven', 91),
+('Student', 'Eight', 89),
+('Student', 'Nine', 93),
+('Student', 'Ten', 90);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Student (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    last_name VARCHAR(255),
+    grade INT
+);
